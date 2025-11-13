ac27f6fe75e5167e0a02a7ebc84e491302113a62
Piotr Opała <opalapj@gmail.com>
2025-11-13 17:55:13 +0100

    Complete os

---
+++ scripts/os_.py
@@ -0,0 +1,13 @@
+import os
+
+
+def start_app(exec_path):
+    os.startfile(exec_path)
+
+
+def main():
+    start_app("C:/Program Files/Microsoft Office/root/Office16/OUTLOOK.EXE")
+
+
+if __name__ == "__main__":
+    main()
